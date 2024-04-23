df54bbc39c515f5f3527a9167ca52b718d5f5120
Anready <nfc939@gmail.com>
2024-04-23 23:32:14 +0300

    added files

---
+++ install.sh
@@ -0,0 +1,13 @@
+pkg install openjdk-17 apksigner
+
+echo "Installing 'sign'"
+cp "sign" $HOME/..usr/bin/sign
+
+if [ $? -eq 0 ]; then
+    echo "Success"
+else
+    echo "Failed to install command 'sign'"
+    exit 1
+fi
+
+echo "Now you can use command 'sign'"
